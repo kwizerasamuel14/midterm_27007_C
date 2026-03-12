e23b9a6003bc7dbd2990f84b3d206060cd2032f6
Samuel Kwizera <kwizhyb14@gmail.com>
2026-03-12 20:07:07 +0200

    Add main Spring Boot application class

---
+++ src/main/java/com/fieldmanagement/system/FieldEmployeeManagementApplication.java
@@ -0,0 +1,12 @@
+package com.fieldmanagement.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FieldEmployeeManagementApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(FieldEmployeeManagementApplication.class, args);
+    }
+}
